81661ea55d03230b8722d1c2cd288e5ccef39970
Grey <125029422+Greyfs@users.noreply.github.com>
2024-04-27 20:38:36 +0700

    Update Navbar.jsx
    Ganti warna

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <Box bg={useColorModeValue("white", "gray.800")} px={4} boxShadow="md">
       <Flex h={16} alignItems="center" justifyContent="space-between" maxW="7xl" mx="auto">
-        <Text fontSize="2xl" fontWeight="bold" color={useColorModeValue("red.500", "red.300")}>
+        <Text fontSize="2xl" fontWeight="bold" color={useColorModeValue("red.500", "red.500")}>
           PELPA
         </Text>
         <Flex>
