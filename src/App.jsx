3b36f65452faee0452542dc2551fa3034032cba7
Grey <125029422+Greyfs@users.noreply.github.com>
2024-04-28 00:38:11 +0700

    Update App.jsx
    Update login dan dashboard slash

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Index from "./pages/Index.jsx";
+import Login from "./pages/login.jsx"
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Index />} />
+        <Route exact path="/login" element={<Login />} /> {/* Tambahkan rute untuk halaman login */}
+        <<Route exact path="/dashboard" element={<Dashboard />} /> {/* Tambahkan rute untuk halaman dashboard */}
       </Routes>
     </Router>
   );
