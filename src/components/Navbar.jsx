9d9e4396da42aacb06aa60be12948ac990be3530
Grey <125029422+Greyfs@users.noreply.github.com>
2024-04-27 20:26:30 +0700

    Update Navbar.jsx
    Edit tampilan header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,8 +10,8 @@ const Navbar = () => {
   return (
     <Box bg={useColorModeValue("white", "gray.800")} px={4} boxShadow="md">
       <Flex h={16} alignItems="center" justifyContent="space-between" maxW="7xl" mx="auto">
-        <Text fontSize="2xl" fontWeight="bold" color={useColorModeValue("purple.500", "purple.300")}>
-          Verbatim
+        <Text fontSize="2xl" fontWeight="bold" color={useColorModeValue("red.500", "red.300")}>
+          PELPA
         </Text>
         <Flex>
           <IconButton icon={colorMode === "light" ? <FaSun /> : <FaMoon />} onClick={toggleColorMode} variant="ghost" aria-label="Toggle color mode" mr={4} />
@@ -31,9 +31,9 @@ const Navbar = () => {
           </Button>
           <Flex justify="center" w="full">
             <Button as={Link} to="/login" variant="ghost" mr={2}>
-              Sign In
+              Login
             </Button>
-            <Button as={Link} to="/signup" bg="purple.500" color="white" _hover={{ bg: "purple.600" }}>
+            <Button as={Link} to="/signup" bg="red.500" color="white" _hover={{ bg: "red.600" }}>
               Sign Up
             </Button>
           </Flex>
